c55c9b7b9a7cf2a025831cacd941db5aee976c50
Keyur Shah <keyurshah3939@gmail.com>
2024-03-17 12:45:23 +0530

    WIP

--- src/services/order-general.service.go
+++ src/services/order-general.service.go
@@ -1,12 +1,17 @@
 package services
 
-import "github.com/rpsoftech/bullion-server/src/mongodb/repos"
+import (
+	"github.com/rpsoftech/bullion-server/src/interfaces"
+	"github.com/rpsoftech/bullion-server/src/mongodb/repos"
+)
 
 type orderGeneralService struct {
-	eventBus       *eventBusService
-	firebaseDb     *firebaseDatabaseService
-	bullionService *bullionDetailsService
-	orderRepo      *repos.OrderRepoStruct
+	eventBus            *eventBusService
+	firebaseDb          *firebaseDatabaseService
+	bullionService      *bullionDetailsService
+	groupMapService     *tradeUserGroupServiceStruct
+	orderRepo           *repos.OrderRepoStruct
+	productGroupMapRepo *repos.ProductGroupMapRepoStruct
 }
 
 var OrderGeneralService *orderGeneralService
@@ -17,12 +22,48 @@ func init() {
 func getOrderGeneralService() *orderGeneralService {
 	if OrderGeneralService == nil {
 		OrderGeneralService = &orderGeneralService{
-			eventBus:       getEventBusService(),
-			firebaseDb:     getFirebaseRealTimeDatabase(),
-			bullionService: getBullionService(),
-			orderRepo:      repos.OrderRepo,
+			eventBus:            getEventBusService(),
+			firebaseDb:          getFirebaseRealTimeDatabase(),
+			bullionService:      getBullionService(),
+			groupMapService:     getTradeUserGroupService(),
+			orderRepo:           repos.OrderRepo,
+			productGroupMapRepo: repos.ProductGroupMapRepo,
 		}
 		println("Order General Service Initialized")
 	}
 	return OrderGeneralService
+}
+
+func (service *orderGeneralService) ValidateVolumeForGroupMapId(groupMapId string, weight int) (bool, error) {
+	groupMap, err := service.productGroupMapRepo.FindOne(groupMapId)
+	if err != nil {
+		return false, err
+	}
+	if !groupMap.ValidateVolume(weight) {
+		return false, &interfaces.RequestError{
+			StatusCode: 400,
+			Code:       interfaces.ERROR_INVALID_VOLUME,
+			Message:    "Invalid Volume",
+			Name:       "INVALID_REQUEST",
+		}
+	}
+	return true, nil
+}
+
+func (service *orderGeneralService) ValidationOfGroupMapIdAndOrder(groupMapId string, userId string, weight int) (bool, error) {
+	groupMap, err := service.productGroupMapRepo.FindOne(groupMapId)
+
+	if err != nil {
+		return false, err
+	}
+
+	if !groupMap.ValidateVolume(weight) {
+		return false, &interfaces.RequestError{
+			StatusCode: 400,
+			Code:       interfaces.ERROR_INVALID_VOLUME,
+			Message:    "Invalid Volume",
+			Name:       "INVALID_REQUEST",
+		}
+	}
+	return true, nil
 }
